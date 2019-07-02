aec205d975ac3e0e0ccb7d808a65468c77a13223
Vijayb023 <vijaypb024@gmail.com>
2019-07-02 06:13:51 -0400

    python practice

---
+++ Python/Input.py
@@ -0,0 +1,9 @@
+fname = input("first name ")
+lname = input("last name ")
+
+birthday = input("how old are you ")
+
+
+print("My name is "+str(fname)+" "+str(lname))
+print("I am "+ str(birthday)+ " years old")
+
